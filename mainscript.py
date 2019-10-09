8a91c4c5efd10f467d639fb67a766d86e6104c19
Miles Tiley <miles.tiley99@gmail.com>
2019-10-09 11:45:14 +1100

    commit 2

--- mainscript.py
+++ mainscript.py
@@ -2,8 +2,8 @@
 
 n = int(input('Enter a maximum value n:  '))
 
+print('These are less than your chosen n: ' + str(n))
 for i in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]: 
-    print('These are less than your chosen ' + str(n))
     if i<=n:
         print (i)
         
